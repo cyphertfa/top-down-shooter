bc009a9a925790768218543ec3a959d9c02d1a14
cyphertfa <jason_yeung@live.co.uk>
2017-05-06 21:40:05 +0800

    rotate and fire projectile

--- Shooter/Assets/Entities/Movement.cs
+++ Shooter/Assets/Entities/Movement.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+	public float rotationspeed = 500; 
+	private Quaternion objectrotate;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,6 +20,7 @@ public class Movement : MonoBehaviour {
 
         if (xAxis != 0)
         {
+			
             velocity.x = 6f * xAxis;
         }
         else
@@ -37,5 +39,14 @@ public class Movement : MonoBehaviour {
 
         Rigidbody2D body = GetComponent<Rigidbody2D>();
         body.velocity = velocity;
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+		// if statement to stop the rotation to rotate back  when there no input, so that it will stay looking at the directino even thought  there no input
+		//also  it can how much can it rotate with each input (sort of) and how fast it rotate (rotationspeed)
+		if(input != Vector3.zero)
+		{
+			transform.rotation  = Quaternion.LookRotation (input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.x,objectrotate.eulerAngles.x,rotationspeed*Time.deltaTime);
+		}
     }
+
 }
