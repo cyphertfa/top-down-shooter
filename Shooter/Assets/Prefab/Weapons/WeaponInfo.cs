1f02f1af96e7462e3aa5f58bd545a14efb65b2bd
Jack Cusack <xdabzi@gmail.com>
2017-05-11 23:57:55 +0100

    Created required classes for weapon refactoring.

---
+++ Shooter/Assets/Prefab/Weapons/WeaponInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInfo : MonoBehaviour {
+
+    public string WeaponName = "unnamed";
+
+    /// <summary>
+    /// If the game object is equipped by a player or enemy this value will be populated.
+    /// </summary>
+    public GameObject Weilder = null;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- Shooter/Assets/Script/WeaponSwitch.cs
+++ Shooter/Assets/Script/WeaponSwitch.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour {
-	private Shooting enableShooting;
-	private RaycastShooting enableRaycast; 
+
+    public const int TOTAL_WEAPON_SLOTS = 4;
+
+    /// <summary>
+    /// The currently equipped weapon.
+    /// </summary>
+    private WeaponInfo currentlyEquipped;
+
+    /// <summary>
+    /// The index of the currently held weapon in heldWeapons.
+    /// </summary>
+    private int currentlyEquppedIndex;
+
+    /// <summary>
+    /// List of currently held weapons.
+    /// This should be populated during initialization by searching though this objects children for all WeaponInfo behaviours.
+    /// </summary>
+    private List<WeaponInfo> heldWeapons = new List<WeaponInfo>();
+
+
+	//private Shooting enableShooting;
+	//private RaycastShooting enableRaycast; 
+
 	// Use this for initialization
 	void Start () 
 	{
-		enableShooting = GetComponent<Shooting> ();
-		enableRaycast = GetComponent<RaycastShooting> ();
+		//enableShooting = GetComponent<Shooting> ();
+		//enableRaycast = GetComponent<RaycastShooting> ();
 	}
 	
 	// Update is called once per frame
@@ -17,14 +38,29 @@ public class WeaponSwitch : MonoBehaviour {
 	{
 		if (Input.GetKeyDown (KeyCode.Alpha1)) 
 		{
-			enableShooting.enabled = true; 
-			enableRaycast.enabled = false; 
-		} 
+            //enableShooting.enabled = true; 
+            //enableRaycast.enabled = false; 
+            SwitchWeapon(0);
+
+        } 
 		if (Input.GetKeyDown (KeyCode.Alpha2))
 		{
-			enableShooting.enabled = false; 
-			enableRaycast.enabled = true; 
-		}
+            //enableShooting.enabled = false; 
+            //enableRaycast.enabled = true; 
+            SwitchWeapon(1);
+        }
 		
 	}
+
+    /// <summary>
+    /// Switches to weapon in the current slot index. 
+    /// 
+    /// The field 'Weilder' on the equipped weapons WeaponInfo should be updated with this component when the weapon is equipped.
+    /// The field 'Weilder' on the unequipped weapons WeaponInfo should be set to null.
+    /// </summary>
+    /// <param name="slot">The index of the slot to switch to.</param>
+    public void SwitchWeapon(int slot)
+    {
+
+    }
 }
