25b3fe8f3f5142df4851da63734da96d4ff5190e
cyphertfa <jason_yeung@live.co.uk>
2017-05-12 05:30:07 +0800

    weapon switch
    pressing 1 or 2 will enable and disable script

---
+++ Shooter/Assets/Script/WeaponSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+	private Shooting enableShooting;
+	private RaycastShooting enableRaycast; 
+	// Use this for initialization
+	void Start () 
+	{
+		enableShooting = GetComponent<Shooting> ();
+		enableRaycast = GetComponent<RaycastShooting> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			enableShooting.enabled = true; 
+			enableRaycast.enabled = false; 
+		} 
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+		{
+			enableShooting.enabled = false; 
+			enableRaycast.enabled = true; 
+		}
+		
+	}
+}
